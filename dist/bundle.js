34c63eea4ad5908b4279108a660f5107f3db51d6
David De Castro <segphault@gmail.com>
2023-04-15 04:00:17 +0200

    On essaye de faire fonctionner Babel

--- dist/bundle.js
+++ dist/bundle.js
@@ -7,90 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./assets/js/app.js":
 /*!**************************!*\
   !*** ./assets/js/app.js ***!
   \**************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _log_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./log.js */ \"./assets/js/log.js\");\n/* Pour importer toutes les fonctions */\n//import * as obj from './log.js';\n\n/* Pour importer une fonction */\n\n\n/* Pour en importer plusieurs */\n//import {log,log2} from './log.js';\n\n//console.log(obj);\n(0,_log_js__WEBPACK_IMPORTED_MODULE_0__.log2)('Salut David !');\n\n//# sourceURL=webpack://webpack1/./assets/js/app.js?");
-
-/***/ }),
-
-/***/ "./assets/js/log.js":
-/*!**************************!*\
-  !*** ./assets/js/log.js ***!
-  \**************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"log\": () => (/* binding */ log),\n/* harmony export */   \"log2\": () => (/* binding */ log2)\n/* harmony export */ });\nfunction log(value) {\n    console.log(value);\n}\nfunction log2(value) {\n    console.log(value);\n    console.log(value);\n}\n\n//# sourceURL=webpack://webpack1/./assets/js/log.js?");
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module 'fs/promises'\\nRequire stack:\\n- /var/www/html/nodejs/webpack1/node_modules/babel-loader/lib/cache.js\\n- /var/www/html/nodejs/webpack1/node_modules/babel-loader/lib/index.js\\n- /var/www/html/nodejs/webpack1/node_modules/loader-runner/lib/loadLoader.js\\n- /var/www/html/nodejs/webpack1/node_modules/loader-runner/lib/LoaderRunner.js\\n- /var/www/html/nodejs/webpack1/node_modules/webpack/lib/NormalModuleFactory.js\\n- /var/www/html/nodejs/webpack1/node_modules/webpack/lib/Compiler.js\\n- /var/www/html/nodejs/webpack1/node_modules/webpack/lib/webpack.js\\n- /var/www/html/nodejs/webpack1/node_modules/webpack/lib/index.js\\n- /var/www/html/nodejs/webpack1/node_modules/webpack-cli/lib/webpack-cli.js\\n- /var/www/html/nodejs/webpack1/node_modules/webpack-cli/lib/bootstrap.js\\n- /var/www/html/nodejs/webpack1/node_modules/webpack-cli/bin/cli.js\\n- /var/www/html/nodejs/webpack1/node_modules/webpack/bin/webpack.js\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:815:15)\\n    at Function.Module._load (internal/modules/cjs/loader.js:667:27)\\n    at Module.require (internal/modules/cjs/loader.js:887:19)\\n    at require (internal/modules/cjs/helpers.js:74:18)\\n    at Object.<anonymous> (/var/www/html/nodejs/webpack1/node_modules/babel-loader/lib/cache.js:24:5)\\n    at Module._compile (internal/modules/cjs/loader.js:999:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1027:10)\\n    at Module.load (internal/modules/cjs/loader.js:863:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:708:14)\\n    at Module.require (internal/modules/cjs/loader.js:887:19)\");\n\n//# sourceURL=webpack://webpack1/./assets/js/app.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./assets/js/app.js");
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./assets/js/app.js"]();
 /******/ 	
 /******/ })()
 ;
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,53 @@
 const path = require('path');
 /* Pour générer une seule configuration */
 
-// module.exports = {
-//     mode: 'development',
-//     entry: './assets/js/app.js',
-//     output: {
-//         path: path.resolve(__dirname, 'dist'),
-//         filename: 'bundle.js',
-//     },
-// };
+module.exports = {
+    mode: 'development',
+    entry: './assets/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: ['babel-loader']
+            }
+        ]
+    }
+};
 
 /* Pour générer multiple configuration */
 
-module.exports = [
-    {
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'production.js',
-            // libraryTarget: 'amd',
-        },
-        // name: 'amd',
-        entry: './assets/js/app.js',
-        watch: true,
-        watchOptions: {
-            aggregateTimeout: 4000,
-        },
-        mode: 'production',
-    },
-    {
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'development.js',
-            // libraryTarget: 'commonjs',
-        },
-        // name: 'commonjs',
-        entry: './assets/js/app.js',
-        watch: true,
-        watchOptions: {
-            aggregateTimeout: 4000,
-        },
-        mode: 'development',
-    },
-]
+// module.exports = [
+//     {
+//         output: {
+//             path: path.resolve(__dirname, 'dist'),
+//             filename: 'production.js',
+//             // libraryTarget: 'amd',
+//         },
+//         // name: 'amd',
+//         entry: './assets/js/app.js',
+//         watch: true,
+//         watchOptions: {
+//             aggregateTimeout: 4000,
+//         },
+//         mode: 'production',
+//     },
+//     {
+//         output: {
+//             path: path.resolve(__dirname, 'dist'),
+//             filename: 'development.js',
+//             // libraryTarget: 'commonjs',
+//         },
+//         // name: 'commonjs',
+//         entry: './assets/js/app.js',
+//         watch: true,
+//         watchOptions: {
+//             aggregateTimeout: 4000,
+//         },
+//         mode: 'development',
+//     },
+// ]
