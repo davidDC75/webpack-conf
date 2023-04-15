bcae4e9a096e61c1c01dfe605e23da16bb2899e4
David De Castro <segphault@gmail.com>
2023-04-15 03:25:56 +0200

    Ajout de script dans package.json pour appeller des alias avec: npm run et ajout de multiple fichiers de configuration webpack

---
+++ webpack-prod-watch.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+/* Pour générer une seule configuration */
+
+module.exports = {
+    mode: 'production',
+    entry: './assets/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'production.js',
+    },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 4000,
+    }
+};
+
+/* Pour générer multiple configuration */
+
+// module.exports = [
+//     {
+//         mode: 'production',
+//         output: {
+//             path: path.resolve(__dirname, 'dist'),
+//             filename: 'production.js',
+//             // libraryTarget: 'amd',
+//         },
+//         // name: 'amd',
+//         entry: './assets/js/app.js',
+//         watch: true,
+//         watchOptions: {
+//             aggregateTimeout: 4000,
+//         },
+//     },
+//     {
+//         mode: 'development',
+//         output: {
+//             path: path.resolve(__dirname, 'dist'),
+//             filename: 'development.js',
+//             // libraryTarget: 'commonjs',
+//         },
+//         // name: 'commonjs',
+//         entry: './assets/js/app.js',
+//         watch: true,
+//         watchOptions: {
+//             aggregateTimeout: 4000,
+//         },
+//     },
+// ]
