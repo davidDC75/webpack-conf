b43724164a2a928437d82f507584c0ee941882bb
David De Castro <segphault@gmail.com>
2023-04-16 17:02:02 +0200

    Test precss dans postcss.config.js, mais deprecated

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     plugins: [
       [
+        // https://www.npmjs.com/package/precss
+        // DEPRECATED
+        // "precss",
+        // https://www.npmjs.com/package/autoprefixer
         "autoprefixer",
         {
           // Options
