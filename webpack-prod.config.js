b14e9bb41956bbbfdd04b27a304b76b82d4123b0
David De Castro <segphault@gmail.com>
2023-04-16 13:22:12 +0200

    Changement du nom du js généré en prod en ajoutant un hash

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -18,8 +18,8 @@ module.exports = {
     entry: './assets/js/app.js', // L'entry
     output: {
         path: path.resolve(__dirname, 'dist'), // Le chemin absolue du répertoire de destination
-        // filename: '[fullhash]-production.js', // Le nom du fichier de sortie avec un hash
-        filename: 'production.js',
+        filename: '[name]-[fullhash]-production.js', // Le nom du fichier de sortie avec un hash
+        // filename: 'production.js',
     },
     // Utilisation de TersetPlugin() pour la minimification
     optimization: {
